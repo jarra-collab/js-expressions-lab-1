5188628ff8ffa6dc07dcbdf173a7b6fe3484fabf
abdiaziz <abdiazizabdullahi40@gmail.com>
2025-11-24 15:01:34 -0800

    lab expressions

--- index.js
+++ index.js
@@ -14,9 +14,64 @@
 //! After creating the four variables mentioned above, uncomment the following lines
 //* This way you can export them to the test file, this is essential for the tests to work
 
-module.exports = {
+//module.exports = {
     // tot_temperature_in_fahrenheit,
     // tot_temperature_in_celsius,
     // avg_temperature_in_fahrenheit,
     // avg_temperature_in_celsius
+};
+// Temperature data (C = Celsius, F = Fahrenheit)
+let temps = [
+  { value: 18, unit: "C" },
+  { value: 70, unit: "F" },
+  { value: 22, unit: "C" },
+  { value: 75, unit: "F" },
+  { value: 20, unit: "C" },
+  { value: 68, unit: "F" },
+  { value: 21, unit: "C" },
+  { value: 73, unit: "F" },
+  { value: 19, unit: "C" },
+  { value: 77, unit: "F" }
+];
+
+// Conversion functions
+function cToF(celsius) {
+  return (celsius * 9/5) + 32;
+}
+
+function fToC(fahrenheit) {
+  return (fahrenheit - 32) * 5/9;
+}
+
+// Totals
+let tot_temperature_in_celsius = 0;
+let tot_temperature_in_fahrenheit = 0;
+
+// Loop through data
+for (let temp of temps) {
+  if (temp.unit === "F") {
+    let c = fToC(temp.value);
+    tot_temperature_in_celsius += c;
+    tot_temperature_in_fahrenheit += temp.value;
+  } else {
+    let f = cToF(temp.value);
+    tot_temperature_in_fahrenheit += f;
+    tot_temperature_in_celsius += temp.value;
+  }
+}
+
+// Averages
+let avg_temperature_in_celsius = tot_temperature_in_celsius / temps.length;
+let avg_temperature_in_fahrenheit = tot_temperature_in_fahrenheit / temps.length;
+
+// Optional: log results
+console.log("Average in °C:", avg_temperature_in_celsius);
+console.log("Average in °F:", avg_temperature_in_fahrenheit);
+
+// Export for testing
+module.exports = {
+  tot_temperature_in_fahrenheit,
+  tot_temperature_in_celsius,
+  avg_temperature_in_fahrenheit,
+  avg_temperature_in_celsius
 };
